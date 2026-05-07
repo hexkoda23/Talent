705eb43d2da97b14f30ca4231eb4745dde999303
Adeleke Kehinde <hexkoda@gmail.com>
2026-05-07 10:06:03 +0100

    fix: add LocalStorageService to StorageModule

--- backend/src/storage/storage.module.ts
+++ backend/src/storage/storage.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AzureBlobStorageService } from './azure-blob-storage.service';
+import { LocalStorageService } from './local-storage.service';
 
 @Module({
-  providers: [AzureBlobStorageService],
-  exports: [AzureBlobStorageService],
+  providers: [AzureBlobStorageService, LocalStorageService],
+  exports: [AzureBlobStorageService, LocalStorageService],
 })
 export class StorageModule {}
